278763a558ff72f084e7f46f67261d18f2feef84
Jacob Huber <jacobnhuber@gmail.com>
2018-10-26 09:45:23 -0500

    simple operations

---
+++ ExploreIntegerMath/ExploreIntegerMath/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreIntegerMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // store values and compute
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            // int c = a - b;
+            // int c = a * b;
+            // int c = a / b;
+            // int c = a % b;
+            Console.WriteLine(c);
+        }
+    }
+}
