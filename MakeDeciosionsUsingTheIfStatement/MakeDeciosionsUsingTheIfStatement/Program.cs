1d2eaf6d1eb7fc5f6bd152efed8f14ac425c96a3
Jacob Huber <jacobnhuber@gmail.com>
2018-10-29 21:15:37 -0500

    simple if statement

---
+++ MakeDeciosionsUsingTheIfStatement/MakeDeciosionsUsingTheIfStatement/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeDeciosionsUsingTheIfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+        }
+    }
+}
